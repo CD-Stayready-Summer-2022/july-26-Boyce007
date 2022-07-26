3132b1a24015655f83e179a446500866a38200fa
Boyce007 <diboyce21@students.desu.edu>
2022-07-26 09:27:57 -0400

    Done

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,25 @@
 package com.codedifferently;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+
+        Map<Character,Integer> map = new TreeMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            if(map.containsKey(input.charAt(i))) {
+                Integer value = map.get(input.charAt(i)) +1;
+                map.put(input.charAt(i),value);
+            } else {
+                map.put(input.charAt(i),1);
+            }
+        }
+        String letters = "";
+        for (Character key:map.keySet()) {
+            letters+= String.format("%s:%d ",key,map.get(key));
+        }
+
+        return letters.trim();
     }
 }
